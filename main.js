209d5458d5229e7cbb24c8673874a4b21f35f52a
Patrick <patrick.hurley@codeimmersives.com>
2020-02-19 15:25:55 -0500

    committed

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
-
-
-
-
+function getFirstItemFrom(array){
+return array[0];
+}
+function getLastItemFrom(array){
+  return array[array.length -1];
+}
+function getIndex3 (array){
+  if ((array.length) < 4){
+    return array[array.length -1];
+  }
+  return array[3];
+}
+function isLongList(array){
+  if (array.length >= 10){
+    return true;
+  } else {
+    return false;
+  }
+}
+function firstItemIsNumber(array){
+  if(typeof array[0] === 'number' ){
+    return true;
+  }else {
+      return false;
+    }
+  }
+function secondCharOfThirdString(array){
+  return array[2][1];
+}
 
 
  /********************************************
